73e330bcfed22e6e800a8edf5d072b51e06eccac
yeonwoo <jangyeonu64@gmail.com>
2026-09-21 11:50:56 +0900

    그레이스케일과 이진화 사진 만들기

---
+++ test2.py
@@ -0,0 +1,10 @@
+import cv2
+color = cv2.imread('road.jpg')
+gray = cv2.imread('road.jpg', cv2.IMREAD_GRAYSCALE)
+if color is None or gray is None:
+    raise FileNotFoundError('road.jpg')
+cv2.imshow('COLOR',color)
+cv2.imshow('GRAY',gray)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
