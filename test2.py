8e946cea0566e4083fe4d090a1b6f0934ddaebb6
yeonwoo <jangyeonu64@gmail.com>
2026-09-23 10:48:38 +0900

    흑백 처리 및 저장 구현

--- test2.py
+++ test2.py
@@ -1,10 +1,17 @@
 import cv2
 color = cv2.imread('road.jpg')
 gray = cv2.imread('road.jpg', cv2.IMREAD_GRAYSCALE)
-if color is None or gray is None:
-    raise FileNotFoundError('road.jpg')
-cv2.imshow('COLOR',color)
-cv2.imshow('GRAY',gray)
+grayPath = 'road_gray.jpg'
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+saveResult = cv2.imwrite(grayPath, gray) # saveResult : 저장 성공 여부
+
+if saveResult:
+    print(f'Image saved successfully at {grayPath}')
+    gray2 = cv2.imread(grayPath)
+    cv2.imshow('GRAY',gray2)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+else:
+    print('Fail to save image')
+# ok = cv2.imwrite('path', gray) # ok : 저장 성공 여부
+# chechk = cv2.imread('path', cv2.IMREAD_GRAYSCALE) # check : 저장된 이미지 읽기
