1247dedd8732899a3aa64fa3af7ac2255a2af012
yeonwoo <jangyeonu64@gmail.com>
2026-09-23 10:56:32 +0900

    채널 확인 추가

--- test2.py
+++ test2.py
@@ -7,7 +7,13 @@ saveResult = cv2.imwrite(grayPath, gray) # saveResult : 저장 성공 여부
 
 if saveResult:
     print(f'Image saved successfully at {grayPath}')
-    gray2 = cv2.imread(grayPath)
+    # gray2 = cv2.imread(grayPath) #채널 수 3개
+    gray2 = cv2.imread(grayPath, cv2.IMREAD_GRAYSCALE) # 채널 수 1개로 만들기
+    print('shape:', gray2.shape) # gray2 = cv2.imread(grayPath) 이거 일 때는 채널 수 3개
+    print('gray dhape', gray.shape)
+    # 기능구현
+
+
     cv2.imshow('GRAY',gray2)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
